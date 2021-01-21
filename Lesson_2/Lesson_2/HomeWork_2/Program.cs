959c2ee5c87943c84a9df7a056bdcf1a9e7ff44d
Pirozhok1985 <pirozhok1985@gmail.com>
2021-01-21 21:34:55 +1000

    Реализовал 3 класса сотрудников и метод расчёта зарплат и реализовал интерфейс IComparable

---
+++ Lesson_2/Lesson_2/HomeWork_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HomeWork_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] arr = {
+                new fpEmployee("Вася", "Пупкин", 1234),
+                new fpEmployee("Петя", "Васечкин", 1235),
+                new fpEmployee("Илья", "Курякин", 1236),
+                new phpEmployee("Михаил", "Хлыстов", 2234, 500),
+                new phpEmployee("Леонид", "Пальчиков", 2235, 600)
+            };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i].Display();
+            }
+            Array.Sort(arr);
+            Console.WriteLine();
+            Console.WriteLine();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i].Display();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lesson_2/Lesson_2/HomeWork_2/phpEmployee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork_2
+{
+    class phpEmployee : Employee
+    {
+        public double PhSalary { get; private set; }
+        public double PerHourPayment { get; private set; }
+        public phpEmployee(string name, string sname, int number, double payment)
+            : base(name, sname, number)
+        {
+            _Name = name;
+            _sName = sname;
+            _tNumber = number;
+            PerHourPayment = payment;
+        }
+
+        public override void SalaryCount(double salary)
+        {
+            PhSalary = 20.8 * 8 * PerHourPayment;
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine($"{_Name}, {_sName}, {_tNumber}");
+        }
+    }
+}
