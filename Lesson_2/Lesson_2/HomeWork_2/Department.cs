2ba04b910ed4b76efbca2a4096280dd81444dda5
Pirozhok1985 <pirozhok1985@gmail.com>
2021-01-21 22:29:07 +1000

    Класс Department с реализацией интерфейсва IEnumerable

---
+++ Lesson_2/Lesson_2/HomeWork_2/Department.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace HomeWork_2
+{
+    class Department : IEnumerable
+    {
+        public int Length { get; private set; }
+        private Employee[] arr;
+
+        public Department(int count)
+        {
+            Length = count;
+            arr = new Employee[Length];
+        }
+        public Employee this[int index]
+        {
+            get
+            {
+                return arr[index];
+            }
+            set
+            {
+                arr[index] = value;
+            }
+        }
+
+
+
+        public IEnumerator GetEnumerator()
+        {
+            return arr.GetEnumerator();
+        }
+    }
+}
--- Lesson_2/Lesson_2/HomeWork_2/Program.cs
+++ Lesson_2/Lesson_2/HomeWork_2/Program.cs
@@ -6,23 +6,34 @@ namespace HomeWork_2
     {
         static void Main(string[] args)
         {
-            Employee[] arr = {
-                new fpEmployee("Вася", "Пупкин", 1234),
-                new fpEmployee("Петя", "Васечкин", 1235),
-                new fpEmployee("Илья", "Курякин", 1236),
-                new phpEmployee("Михаил", "Хлыстов", 2234, 500),
-                new phpEmployee("Леонид", "Пальчиков", 2235, 600)
-            };
-            for (int i = 0; i < arr.Length; i++)
+            //Employee[] arr = {
+            //    new fpEmployee("Вася", "Пупкин", 1234),
+            //    new fpEmployee("Петя", "Васечкин", 1235),
+            //    new fpEmployee("Илья", "Курякин", 1236),
+            //    new phpEmployee("Михаил", "Хлыстов", 2234, 500),
+            //    new phpEmployee("Леонид", "Пальчиков", 2235, 600)
+            //};
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i].Display();
+            //}
+            //Array.Sort(arr);
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i].Display();
+            //}
+
+            Department it = new Department(5);
+            it[0] = new phpEmployee("Михаил", "Хлыстов", 2234, 500);
+            it[1] = new phpEmployee("Леонид", "Пальчиков", 2235, 600);
+            it[2] = new fpEmployee("Илья", "Курякин", 1236);
+            it[3] = new fpEmployee("Петя", "Васечкин", 1235);
+            it[4] = new fpEmployee("Вася", "Пупкин", 1234);
+            foreach (Employee val in it)
             {
-                arr[i].Display();
-            }
-            Array.Sort(arr);
-            Console.WriteLine();
-            Console.WriteLine();
-            for (int i = 0; i < arr.Length; i++)
-            {
-                arr[i].Display();
+                val.Display();
             }
             Console.ReadLine();
         }
