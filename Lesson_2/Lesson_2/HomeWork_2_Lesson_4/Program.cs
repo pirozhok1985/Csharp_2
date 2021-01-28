3b756d86ad5b5dd566e25bf2c9644fe76d3181ee
Pirozhok1985 <pirozhok1985@gmail.com>
2021-01-28 23:53:50 +1000

    Подсчёт элементов в коллекции.

---
+++ Lesson_2/Lesson_2/HomeWork_2_Lesson_4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HomeWork_2_Lesson_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> Pairs = new Dictionary<int, int>();
+            Random rand = new Random();
+            int count = 1;
+            List<int> list = new List<int>(50);
+            for (var i = 0; i < list.Capacity; i++)
+            {
+                list.Add(rand.Next(0,10));
+            }
+
+            list.Sort();
+            for (var i = 0; i < list.Capacity; i++)
+            {
+                if (Pairs.ContainsKey(list[i]))
+                {
+                    Pairs[list[i]] += 1;
+                }
+                else
+                {
+                    Pairs.Add(list[i],count);
+                }
+            }
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+
+            foreach (var element in Pairs)
+            {
+                Console.WriteLine("{0}:{1}",element.Key, element.Value);
+            }
+        }
+    }
+}
