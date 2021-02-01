ffd41f8897a36a1610d7f3c094519c68b36df679
Pirozhok1985 <pirozhok1985@gmail.com>
2021-02-01 19:43:29 +1000

    Создал wpf форму и классы Employee и Department

---
+++ Lesson_2/Lesson_2/HelloWPF/Department.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWPF
+{
+    class Department : IEnumerable
+    {
+        public List<Employee> Dep { get; set; }
+        public string DepName { get; set; }
+
+        public Department(string name)
+        {
+            Dep = new List<Employee>();
+            DepName = name;
+        }
+
+        public Employee this[int index]
+        {
+            get
+            {
+                return Dep[index];
+            }
+
+            set
+            {
+                Dep[index] = value;
+            }
+
+        }
+
+        public void AddEmployee(Employee emp)
+        {
+            Dep.Add(emp);
+            emp.Department = DepName;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return Dep.GetEnumerator();
+        }
+    }
+}
---
+++ Lesson_2/Lesson_2/HelloWPF/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace HelloWPF
+{
+    class Employee
+    {
+        public string Name { get;}
+        public string SName { get;}
+        public int Id { get;}
+        public string Department { get; set; }
+
+        public Employee(string name, string sname, int id)
+        {
+            Name = name;
+            SName = sname;
+            Id = id;
+        }
+
+        public void AddToDepartment(Department dep)
+        {
+            Department = dep.DepName;
+            dep.AddEmployee(this);
+        }
+    }
+}
